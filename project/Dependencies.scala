54de416dc5897ae68fd86d5a02f776bbbec7db6a
Matteo Baglini <matteo.baglini@gmail.com>
2018-06-18 14:34:47 +0200

    Update libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % "1.1.0"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.0.0-RC2"
-  lazy val catsMtl = "org.typelevel" %% "cats-mtl-core" % "0.2.1"
+  lazy val catsMtl = "org.typelevel" %% "cats-mtl-core" % "0.3.0"
   lazy val miniTest = "io.monix" %% "minitest" % "2.1.1"
 }
