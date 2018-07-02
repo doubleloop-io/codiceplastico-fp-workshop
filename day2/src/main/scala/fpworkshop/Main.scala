63f42b098dbb2d7689067412d4e2b6a8beff66fb
Matteo Baglini <matteo.baglini@gmail.com>
2018-07-02 16:58:04 +0200

    Restore empty Main

--- day2/src/main/scala/fpworkshop/Main.scala
+++ day2/src/main/scala/fpworkshop/Main.scala
@@ -1,14 +1,5 @@
 package fpworkshop
 
 object Main extends App {
-
-  import fpworkshop.day2.validation.Final._
-
-  def prova() = {
-    checkNumber("sadf")
-  }
-
-  println(prova())
-
   println("Hello from DAY2")
 }
