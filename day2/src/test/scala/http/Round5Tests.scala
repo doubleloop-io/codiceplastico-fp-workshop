d33c2f62cefaee87ad43382621a7bfcbd2f485c2
Matteo Baglini <matteo.baglini@gmail.com>
2018-07-03 16:39:35 +0200

    Remove garbage

--- day2/src/test/scala/http/Round5Tests.scala
+++ day2/src/test/scala/http/Round5Tests.scala
@@ -9,14 +9,6 @@ import day2.http.solutions.Round5._
 
 object Round5Tests extends SimpleTestSuite {
 
-  testAsync("asynchronous execution") {
-    val future = Future(100).map(_ + 1)
-
-    for (result <- future) yield {
-      assertEquals(result, 101)
-    }
-  }
-
   testAsync("match the /hello route") {
     val req = Request(POST, Uri("/hello"), "matteo")
     for (res <- app(req))
