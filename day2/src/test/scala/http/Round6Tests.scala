83b1c0657a0d0cfd219bea515b8be94afbfb71f5
Matteo Baglini <matteo.baglini@gmail.com>
2018-07-03 16:40:11 +0200

    Add round 6 for http

---
+++ day2/src/test/scala/http/Round6Tests.scala
@@ -0,0 +1,30 @@
+package day2.http
+
+import minitest._
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import day2.http.solutions.Round6._
+
+object Round6Tests extends SimpleTestSuite {
+
+  testAsync("match the /hello route") {
+    val req = Request(POST, Uri("/hello"), "matteo")
+    for (res <- app(req))
+      yield assertEquals(res, Response(OK, "Hello, matteo!"))
+  }
+
+  testAsync("match the /ciao route") {
+    val req = Request(POST, Uri("/ciao"), "matteo")
+    for (res <- app(req))
+      yield assertEquals(res, Response(OK, "Ciao, matteo!"))
+  }
+
+  testAsync("fallback route") {
+    val req = Request(POST, Uri("/not-hello"), "matteo")
+    for (res <- app(req))
+      yield assertEquals(Response(NotFound), res)
+  }
+
+}
