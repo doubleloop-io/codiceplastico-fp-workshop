8f98998961e02504991fd235cdeab0e237b9b141
Matteo Baglini <matteo.baglini@gmail.com>
2018-07-04 11:37:52 +0200

    Add round 9 for http

---
+++ day2/src/main/scala/http/solutions/Round9.scala
@@ -0,0 +1,60 @@
+package day2.http.solutions
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import cats._
+import cats.data._
+import cats.implicits._
+
+import day2.http._
+
+object Round9 {
+  // GOAL: Remove custom combine
+
+  object Translator {
+
+    def italian[F[_]: Monad](text: String): F[String] =
+      Monad[F].pure(text match {
+        case "Hello, matteo!" => s"Ciao, matteo!"
+      })
+  }
+
+  type Http[F[_]] = Kleisli[F, Request, Response]
+  type HttpApp = Http[Future]
+  type HttpRoutes = Http[OptionT[Future, ?]]
+
+  object HttpApp {
+    def apply(f: Request => Future[Response]): HttpApp =
+      Kleisli(f)
+  }
+
+  object HttpRoutes {
+    def of(pf: PartialFunction[Request, Future[Response]]): HttpRoutes =
+      Kleisli(req => OptionT(pf.lift(req).sequence))
+  }
+
+  def greet(theUri: Uri): HttpRoutes = HttpRoutes.of {
+    case Request(POST, uri, name) if uri == theUri =>
+      Future.successful(Response(OK, s"Hello, $name!"))
+  }
+
+  val hello: HttpRoutes = greet(Uri("/hello"))
+  val ciao: HttpRoutes = translate(greet(Uri("/ciao")))
+
+  val app: HttpApp = seal(hello.combineK(ciao))
+  val appSymbol: HttpApp = seal(hello <+> ciao)
+  val appTranslateOnRoute: HttpApp = seal(translate(hello))
+  val appTranslateOnApp: HttpApp = translate(seal(hello))
+
+  def seal(routes: HttpRoutes): HttpApp =
+    routes.mapF(_.getOrElseF(Future.successful(Response(NotFound))))
+
+  def translate[F[_]: Monad](http: Http[F]): Http[F] =
+    http.flatMap(
+      res =>
+        Kleisli
+          .liftF(Translator.italian[F](res.body))
+          .map(ita => res.copy(body = ita))
+    )
+}
