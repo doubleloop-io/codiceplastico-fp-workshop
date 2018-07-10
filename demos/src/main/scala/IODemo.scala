5bb0a31959ae6c374a5d4b455b380b6047ac2b37
Matteo Baglini <matteo.baglini@gmail.com>
2018-07-10 17:34:04 +0200

    fix demos and move here IO

---
+++ demos/src/main/scala/IODemo.scala
@@ -0,0 +1,32 @@
+package demos
+
+object IODemo {
+
+  class IO[A] private (op: () => A) {
+    def unsafePerformIO(): A = op()
+
+    def flatMap[B](f: A => IO[B]): IO[B] =
+      IO(f(unsafePerformIO()).unsafePerformIO())
+
+    def map[B](f: A => B): IO[B] =
+      flatMap(a => IO(f(a)))
+
+    def attempt(): IO[Either[Throwable, A]] =
+      IO(
+        try Right(unsafePerformIO())
+        catch { case e: Throwable => Left(e) }
+      )
+  }
+
+  object IO {
+    def unit(): IO[Unit] =
+      IO(())
+
+    def apply[A](op: => A): IO[A] =
+      new IO[A](() => op)
+
+    def fail[A](e: Throwable): IO[A] =
+      IO(throw e)
+  }
+
+}
--- demos/src/main/scala/LensDemo.scala
+++ demos/src/main/scala/LensDemo.scala
@@ -1,19 +1,19 @@
 package demos
 
-object LensDemo{
+object LensDemo {
 
-case class Lens[S, A](get: S => A, set: (A, S) => S) {
+  case class Lens[S, A](get: S => A, set: (A, S) => S) {
 
-  def composeLens[B](that: Lens[A, B]): Lens[S, B] =
-    composeLens(that)
+    def |->[B](that: Lens[A, B]): Lens[S, B] =
+      composeLens(that)
 
-  def composeLens[B](that: Lens[A, B]): Lens[S, B] = Lens(
-    get = that.get.compose(get),
-    set = (b, s) => set(that.set(b, get(s)), s)
-  )
+    def composeLens[B](that: Lens[A, B]): Lens[S, B] = Lens(
+      get = that.get.compose(get),
+      set = (b, s) => set(that.set(b, get(s)), s)
+    )
 
-  def modify(f: A => A): S => S =
-    s => set(f(get(s)), s)
-}
+    def modify(f: A => A): S => S =
+      s => set(f(get(s)), s)
+  }
 
 }
