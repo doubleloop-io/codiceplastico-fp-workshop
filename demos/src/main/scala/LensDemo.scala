e42bf4d8d459dc81738775681a081d7431221444
Matteo Baglini <matteo.baglini@gmail.com>
2018-07-17 13:22:18 +0200

    More demo lens

--- demos/src/main/scala/LensDemo.scala
+++ demos/src/main/scala/LensDemo.scala
@@ -16,4 +16,16 @@ object LensDemo {
       s => set(f(get(s)), s)
   }
 
+  case class House(person: Person)
+  case class Person(name: String, age: Int)
+
+  val _person = Lens[House, Person](_.person, (v, old) => old.copy(person = v))
+  val _name   = Lens[Person, String](_.name, (v, old) => old.copy(name = v))
+
+  val p = Person("matteo", 35)
+  val h = House(p)
+
+  val n = _person |-> _name
+
+  val newHouse = n.modify(old => old + " baglini")(h)
 }
